f823ee01a2295d8c49957c7be30f32f32f495f38
Shivam <shivamchandak9@gmail.com>
2024-04-09 00:44:19 +0530

    fix baseurl

--- src/Components/Api.js
+++ src/Components/Api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-// const baseURL = "https://ccl-mini-project.onrender.com/";
-const baseURL = "http://localhost:3177";
+const baseURL = "https://ccl-mini-project.onrender.com/";
+// const baseURL = "http://localhost:3177";
 
 export default axios.create({ baseURL: baseURL });
